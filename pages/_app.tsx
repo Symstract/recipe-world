779347064f674ef7cb378d8092bea27ec8eec8c5
Antti Tikka <antti.tikka@hotmail.com>
2022-06-23 02:51:02 +0300

    Add theme, fonts and global styles to custom app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+
+import "@fontsource/carlito";
+import "@fontsource/tienne";
+
+import GlobalStyles from "../components/GlobalStyles";
+import theme from "../theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
