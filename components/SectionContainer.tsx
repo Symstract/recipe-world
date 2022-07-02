873c1ddcc87e6da2c66ff5109a0d3d05daf8744d
Antti Tikka <antti.tikka@hotmail.com>
2022-07-03 01:06:53 +0300

    Make SectionContainer responsive

--- components/SectionContainer.tsx
+++ components/SectionContainer.tsx
@@ -1,9 +1,18 @@
 import styled from "styled-components";
 
 const SectionContainer = styled.div`
-  padding: 0 ${({ theme }) => theme.pageWidths.mobile.padding};
   width: 100%;
   height: 100%;
+  max-width: calc(
+    ${({ theme }) => theme.pageWidths.desktop.maxWidth} +
+      ${({ theme }) => theme.pageWidths.tablet.padding} * 2
+  );
+  margin: 0 auto;
+  padding: 0 ${({ theme }) => theme.pageWidths.mobile.padding};
+
+  @media screen and (min-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    padding: 0 ${({ theme }) => theme.pageWidths.tablet.padding};
+  }
 `;
 
 export default SectionContainer;
