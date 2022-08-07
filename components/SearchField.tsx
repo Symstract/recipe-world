5947087b992a909fe2ca3dd6ede06b6cf19ef967
Antti Tikka <antti.tikka@hotmail.com>
2022-08-07 03:28:57 +0300

    Implement intial search autocompletion

--- components/SearchField.tsx
+++ components/SearchField.tsx
@@ -1,9 +1,13 @@
 import React, {
   forwardRef,
+  useCallback,
+  useEffect,
   useImperativeHandle,
+  useMemo,
   useRef,
   useState,
 } from "react";
+import ReactDOM from "react-dom";
 import styled, { css, useTheme } from "styled-components";
 
 import { addOpacityToHexColor } from "lib/colorUtils";
@@ -92,6 +96,160 @@ const SearchForm = styled.form<SearchProps>`
   ${(props) => props.searchStyle === "navbar" && searchFormStyleNavbar};
 `;
 
+// === Search suggestions ===
+
+interface ISearchSuggestion {
+  suggestionId: string;
+  suggestionPhrase: string;
+}
+
+interface SearchSuggestionProps
+  extends ISearchSuggestion,
+    React.ButtonHTMLAttributes<HTMLButtonElement> {
+  searchStyle: SearchStyle;
+  isHighlighted: boolean;
+}
+
+const StyledSearchSuggestion = styled.button<SearchSuggestionProps>`
+  width: 100%;
+  padding: 0.6rem ${({ theme }) => theme.pageWidths.mobile.padding};
+  background: ${(props) =>
+    props.isHighlighted ? props.theme.colors.surfaceHighlight : "none"};
+  font-size: ${(props) =>
+    props.searchStyle === "navbar" ? "inherit" : "2rem"};
+  text-align: left;
+
+  @media screen and (min-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    padding: 0.6rem 1.2rem;
+  }
+`;
+
+function SearchSuggestion(props: SearchSuggestionProps) {
+  return (
+    <StyledSearchSuggestion {...props}>
+      {props.suggestionPhrase}
+    </StyledSearchSuggestion>
+  );
+}
+
+interface SearchSuggestionListProps {
+  searchStyle: SearchStyle;
+  searchPhrase: string;
+  suggestions: ISearchSuggestion[];
+  setSuggestions: React.Dispatch<React.SetStateAction<ISearchSuggestion[]>>;
+  highlightedIndex: number | null;
+  setHighlightedIndex: React.Dispatch<React.SetStateAction<number | null>>;
+  onClick: () => void;
+  formRef: React.RefObject<HTMLFormElement>;
+}
+
+const StyledSearchSuggestionList = styled.ul`
+  position: absolute;
+  z-index: 999;
+  border-top: 1px solid ${({ theme }) => theme.colors.textSecondary};
+  border-bottom: 1px solid ${({ theme }) => theme.colors.textSecondary};
+  background: ${(props) => props.theme.colors.surface};
+
+  @media screen and (min-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    border: 1px solid ${({ theme }) => theme.colors.textSecondary};
+    border-top: none;
+  }
+`;
+
+function SearchSuggestionList(props: SearchSuggestionListProps) {
+  const {
+    searchStyle,
+    formRef,
+    onClick,
+    searchPhrase,
+    highlightedIndex,
+    setHighlightedIndex,
+    setSuggestions,
+    suggestions,
+  } = props;
+
+  // Intial test data
+  const suggestions_: ISearchSuggestion[] = useMemo(() => {
+    return [
+      { suggestionId: "df3aef", suggestionPhrase: "dgdfdfg" },
+      { suggestionId: "dfa", suggestionPhrase: "dgdfdfg sdfwe" },
+      { suggestionId: "dffgncv", suggestionPhrase: "dgdfdfg er5yfbg" },
+      { suggestionId: "4tdfgfg", suggestionPhrase: "dgdfdfg sdae sdfsf" },
+    ];
+  }, []);
+
+  useEffect(() => {
+    // Fetch suggestions here
+    setSuggestions(suggestions_);
+  }, [searchPhrase, setSuggestions, suggestions_]);
+
+  const theme = useTheme();
+  const listRef = useRef<HTMLUListElement>(null);
+
+  const setStyle = useCallback(() => {
+    if (!formRef.current || !listRef.current) return;
+
+    const formRect = formRef.current.getBoundingClientRect();
+    const tabletBreakpoint = +theme.breakpoints.tablet.slice(
+      0,
+      theme.breakpoints.tablet.length - 2
+    );
+    const listStyle = listRef.current.style;
+
+    listStyle.top = formRect.bottom + "px";
+
+    if (window.innerWidth < tabletBreakpoint) {
+      listStyle.left = "0";
+      listStyle.width = "100%";
+    } else {
+      listStyle.left = formRect.left + "px";
+      listStyle.width = formRect.right - formRect.left + "px";
+    }
+  }, [formRef, theme.breakpoints.tablet]);
+
+  useEffect(() => {
+    setStyle();
+    window.addEventListener("resize", setStyle);
+
+    return () => window.removeEventListener("resize", setStyle);
+  });
+
+  const handlePointerMove = (e: React.PointerEvent<HTMLLIElement>) => {
+    const items = Array.from(e.currentTarget.parentElement!.children);
+    const index = items.findIndex((elem) => elem === e.currentTarget);
+    setHighlightedIndex(index === -1 ? null : index);
+  };
+
+  const handlePointerLeave = () => {
+    setHighlightedIndex(null);
+  };
+
+  const list = (
+    <StyledSearchSuggestionList ref={listRef}>
+      {suggestions_.map((su, index) => (
+        <li
+          key={su.suggestionId}
+          onPointerMove={handlePointerMove}
+          onPointerLeave={handlePointerLeave}
+        >
+          <SearchSuggestion
+            searchStyle={searchStyle}
+            suggestionId={su.suggestionId}
+            suggestionPhrase={su.suggestionPhrase}
+            isHighlighted={index === highlightedIndex}
+            onClick={onClick}
+          />
+        </li>
+      ))}
+    </StyledSearchSuggestionList>
+  );
+
+  return ReactDOM.createPortal(
+    list,
+    document.getElementById("__next") as HTMLDivElement
+  );
+}
+
 // Main component
 // =============================================================================
 
@@ -107,7 +265,11 @@ export type SearchFieldHandle = {
 const SearchField = forwardRef<SearchFieldHandle, SearchFieldProps>(
   function SearchFieldToForward({ searchStyle = "regular", onBlur }, ref) {
     const [hasFocus, setHasFocus] = useState(false);
-    const [hasInput, setHasInput] = useState(false);
+    const [nonAutocompletedInputValue, setNonAutocompletedInputValue] =
+      useState<string>("");
+    const [suggestions, setSuggestions] = useState<ISearchSuggestion[]>([]);
+    const [highlightedSuggestionIndex, setHighlightedSuggestionIndex] =
+      useState<number | null>(null);
 
     const formRef = useRef<HTMLFormElement>(null);
     const inputRef = useRef<HTMLInputElement>(null);
@@ -125,10 +287,72 @@ const SearchField = forwardRef<SearchFieldHandle, SearchFieldProps>(
 
     const handleBlur = () => {
       setHasFocus(false);
+      setHighlightedSuggestionIndex(null);
     };
 
-    const handleInput = () => {
-      setHasInput(!!inputRef.current?.value.length);
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setNonAutocompletedInputValue(e.target.value);
+      setHighlightedSuggestionIndex(null);
+    };
+
+    const selectNextSuggestion = () => {
+      if (highlightedSuggestionIndex === null) {
+        setHighlightedSuggestionIndex(0);
+        if (inputRef.current) {
+          inputRef.current.value = suggestions[0].suggestionPhrase;
+        }
+        return;
+      }
+
+      if (highlightedSuggestionIndex === suggestions.length - 1) {
+        setHighlightedSuggestionIndex(null);
+        if (inputRef.current) {
+          inputRef.current.value = nonAutocompletedInputValue;
+        }
+        return;
+      }
+
+      setHighlightedSuggestionIndex(highlightedSuggestionIndex + 1);
+      if (inputRef.current) {
+        inputRef.current.value =
+          suggestions[highlightedSuggestionIndex + 1].suggestionPhrase;
+      }
+    };
+
+    const selectPreviousSuggestion = () => {
+      if (highlightedSuggestionIndex === null) {
+        setHighlightedSuggestionIndex(suggestions.length - 1);
+        if (inputRef.current) {
+          inputRef.current.value =
+            suggestions[suggestions.length - 1].suggestionPhrase;
+        }
+
+        return;
+      }
+
+      if (highlightedSuggestionIndex === 0) {
+        setHighlightedSuggestionIndex(null);
+        if (inputRef.current) {
+          inputRef.current.value = nonAutocompletedInputValue;
+        }
+        return;
+      }
+
+      setHighlightedSuggestionIndex(highlightedSuggestionIndex - 1);
+      if (inputRef.current) {
+        inputRef.current.value =
+          suggestions[highlightedSuggestionIndex - 1].suggestionPhrase;
+      }
+    };
+
+    const handleKeyDown = (e: React.KeyboardEvent) => {
+      if (e.key === "ArrowUp") {
+        e.preventDefault();
+        selectPreviousSuggestion();
+      } else if (e.key === "ArrowDown") {
+        e.preventDefault();
+        selectNextSuggestion();
+      }
     };
 
     const commonButtonStyle: ButtonStyle = {
@@ -148,7 +372,8 @@ const SearchField = forwardRef<SearchFieldHandle, SearchFieldProps>(
           searchStyle={searchStyle}
           onFocus={handleFocus}
           onBlur={handleBlur}
-          onInput={handleInput}
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
           ref={inputRef}
         />
         <SearchButtonContainer searchStyle={searchStyle}>
@@ -157,9 +382,21 @@ const SearchField = forwardRef<SearchFieldHandle, SearchFieldProps>(
             minWidthToShowRegularLayout={theme.breakpoints.tablet}
             compactStyle={{ iconSize: "2.4rem", ...commonButtonStyle }}
             regularStyle={{ iconSize: "2.8rem", ...commonButtonStyle }}
-            aria-disabled={!hasInput}
+            aria-disabled={!nonAutocompletedInputValue}
           />
         </SearchButtonContainer>
+        {nonAutocompletedInputValue && hasFocus && (
+          <SearchSuggestionList
+            searchStyle={searchStyle}
+            formRef={formRef}
+            searchPhrase={nonAutocompletedInputValue}
+            onClick={() => {}}
+            highlightedIndex={highlightedSuggestionIndex}
+            setHighlightedIndex={setHighlightedSuggestionIndex}
+            suggestions={suggestions}
+            setSuggestions={setSuggestions}
+          />
+        )}
       </SearchForm>
     );
   }
