44f5cbbfacf1b26e18dc2c87ba9cd22606ae7ef3
Antti Tikka <antti.tikka@hotmail.com>
2022-09-05 20:11:11 +0300

    Cleanup: remove redundant font loading html from _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,12 +5,6 @@ class CustomDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
           <link
             href="https://fonts.googleapis.com/css2?family=Mukta&family=Volkhov:wght@400;700&display=optional"
             rel="stylesheet"
